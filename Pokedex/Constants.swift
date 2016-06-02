cdb57d4fee0f60b1225bb27e1be12f6e987b40b7
Armin Bislig <abisli@bwo-ml16-62987.pvhcorp.com>
2016-06-02 16:13:14 -0400

    we just finished our pokedex app. wooohooo

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Armin Bislig on 6/2/16.
+//  Copyright © 2016 Armin Bislig. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
+
---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Armin Bislig on 6/1/16.
+//  Copyright © 2016 Armin Bislig. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 6.0
+    }
+
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
